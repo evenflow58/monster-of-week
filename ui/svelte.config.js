359b9b46af6e384eabd5a9f745499d06d1a7415f
evenflow58 <evenflow58@gmail.com>
2022-08-17 21:05:26 -0400

    Using aws build adapter

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { adapter } from 'sveltekit-adapter-aws';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +8,9 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			default: true
-		}
+		adapter: adapter({
+			artifactPath: 'build'
+		})
 	}
 };
 
