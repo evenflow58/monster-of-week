b5e1f036a2d05978a4a56c7d031cdcc361b552e3
evenflow58 <evenflow58@gmail.com>
2022-09-16 23:58:48 -0400

    Checking staging

--- infrastructure/url/index.js
+++ infrastructure/url/index.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
   const response = {
     status: 200,
     statusDescription: "OK",
-    body: "Lambda@Edge is awesome!",
+    body: "Lambda@Edge is awesome! Part 2!",
   };
   callback(null, response);
 };
