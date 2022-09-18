486a3e0b0a45418e3107467475b322313be5f46d
evenflow58 <evenflow58@gmail.com>
2022-09-18 09:37:59 -0400

    Checking different domain passing

--- infrastructure/url/index.js
+++ infrastructure/url/index.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
   const response = {
     status: 200,
     statusDescription: "OK",
-    body: "Lambda@Edge is awesome! Part 2!",
+    body: "Lambda@Edge is awesome! Part 3!",
   };
   callback(null, response);
 };
