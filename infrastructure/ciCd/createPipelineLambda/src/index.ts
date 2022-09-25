56b1df226fb46e4ac692134c21271c7c3bae9cae
evenflow58 <evenflow58@gmail.com>
2022-09-25 16:30:32 -0400

    Finishing up create pipeline

--- infrastructure/ciCd/createPipelineLambda/src/index.ts
+++ infrastructure/ciCd/createPipelineLambda/src/index.ts
@@ -1,5 +1,32 @@
-// import {  } from 'aws-lambda';
+import {
+  CloudFormationClient,
+  CreateStackCommand,
+  CreateStackCommandInput,
+} from "@aws-sdk/client-cloudformation";
 
-export const handler = (event: any): any => {
-  console.log("Event", event);
+export const handler = async (event: { BranchName: string }): Promise<any> => {
+  if (event.BranchName === "master") {
+    console.log("Not creating anything because this is the master branch.");
+    return;
+  }
+
+  const client = new CloudFormationClient({ region: "us-east-1" });
+  const input: CreateStackCommandInput = {
+    StackName: `monster-week-${event.BranchName}`,
+    TemplateURL: `https://github.com/evenflow58/monster-of-week/tree/master/infrastructure/pipeline/template.yaml`,
+    Parameters: [
+      {
+        ParameterKey: "BranchName",
+        ParameterValue: event.BranchName,
+        UsePreviousValue: false,
+      },
+    ],
+    OnFailure: "ROLLBACK",
+    Capabilities: ["CAPABILITY_NAMED_IAM"],
+  };
+  const command = new CreateStackCommand(input);
+
+  console.log(`Creating environment for ${event.BranchName}`);
+
+  return await client.send(command);
 };
