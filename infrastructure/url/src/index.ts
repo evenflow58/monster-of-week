b7aa47d1a830cd50e22a42cedab703c23acc4863
evenflow58 <evenflow58@gmail.com>
2022-09-30 18:17:28 -0400

    Connecting to S3 bucket

--- infrastructure/url/src/index.ts
+++ infrastructure/url/src/index.ts
@@ -1,4 +1,22 @@
-export const handler = async (): Promise<any> => {
+import {
+  S3Client,
+  GetObjectCommand,
+  GetObjectCommandInput,
+} from "@aws-sdk/client-s3";
+
+export const handler = async (event: any): Promise<any> => {
+  console.log(event);
+
+  const input: GetObjectCommandInput = {
+    Bucket: "monster-of-the-week-ui",
+    Key: "serve-site-from-edge-lambda/",
+  };
+  const command = new GetObjectCommand(input);
+  const client = new S3Client({ region: "us-east-1" });
+  const files = await client.send(command);
+
+  console.log("got files");
+
   const response = {
     status: 200,
     statusDescription: "OK",
