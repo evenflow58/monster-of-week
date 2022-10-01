ccafa26390fc37dde6e36474f2ab76a105ef0588
evenflow58 <evenflow58@gmail.com>
2022-10-01 14:47:45 -0400

    Paring down code

--- infrastructure/url/src/index.ts
+++ infrastructure/url/src/index.ts
@@ -1,24 +1,24 @@
-import {
-  S3Client,
-  GetObjectCommand,
-  GetObjectCommandInput,
-} from "@aws-sdk/client-s3";
+// import {
+//   S3Client,
+//   GetObjectCommand,
+//   GetObjectCommandInput,
+// } from "@aws-sdk/client-s3";
 
 export const handler = async (event: any): Promise<any> => {
   console.log(event.Records[0].cf.request.origin.custom);
 
-  const input: GetObjectCommandInput = {
-    Bucket: "monster-of-the-week-ui",
-    Key: "serve-site-from-edge-lambda/",
-  };
-  const command = new GetObjectCommand(input);
-  const client = new S3Client({ region: "us-east-1" });
+  // const input: GetObjectCommandInput = {
+  //   Bucket: "monster-of-the-week-ui",
+  //   Key: "serve-site-from-edge-lambda/",
+  // };
+  // const command = new GetObjectCommand(input);
+  // const client = new S3Client({ region: "us-east-1" });
 
-  console.log("getting files");
+  // console.log("getting files");
 
-  const files = await client.send(command);
+  // const files = await client.send(command);
 
-  console.log("got files");
+  // console.log("got files");
 
   const response = {
     status: 200,
