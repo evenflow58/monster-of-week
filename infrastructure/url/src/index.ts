2ac2cda0ae77130dd6a471013d7f539f001c95e2
evenflow58 <evenflow58@gmail.com>
2022-10-01 08:10:29 -0400

    Re-add S3 code

--- infrastructure/url/src/index.ts
+++ infrastructure/url/src/index.ts
@@ -1,21 +1,21 @@
-// import {
-//   S3Client,
-//   GetObjectCommand,
-//   GetObjectCommandInput,
-// } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  GetObjectCommand,
+  GetObjectCommandInput,
+} from "@aws-sdk/client-s3";
 
 export const handler = async (event: any): Promise<any> => {
   console.log(event.Records[0].cf);
 
-  // const input: GetObjectCommandInput = {
-  //   Bucket: "monster-of-the-week-ui",
-  //   Key: "serve-site-from-edge-lambda/",
-  // };
-  // const command = new GetObjectCommand(input);
-  // const client = new S3Client({ region: "us-east-1" });
-  // const files = await client.send(command);
+  const input: GetObjectCommandInput = {
+    Bucket: "monster-of-the-week-ui",
+    Key: "serve-site-from-edge-lambda/",
+  };
+  const command = new GetObjectCommand(input);
+  const client = new S3Client({ region: "us-east-1" });
+  const files = await client.send(command);
 
-  // console.log("got files");
+  console.log("got files");
 
   const response = {
     status: 200,
