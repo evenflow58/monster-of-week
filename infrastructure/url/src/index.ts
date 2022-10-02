6786c67e2616c5e6b385da74a43df88d8c920c00
evenflow58 <evenflow58@gmail.com>
2022-10-02 07:58:54 -0400

    Adding permissions

--- infrastructure/url/src/index.ts
+++ infrastructure/url/src/index.ts
@@ -1,29 +1,29 @@
-// import {
-//   S3Client,
-//   GetObjectCommand,
-//   GetObjectCommandInput,
-// } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  GetObjectCommand,
+  GetObjectCommandInput,
+} from "@aws-sdk/client-s3";
 
 export const handler = async (event: any): Promise<any> => {
-  console.log(event.Records[0].cf.request.origin.custom);
+  console.log(JSON.stringify(event.Records[0].cf.request));
 
-  // const input: GetObjectCommandInput = {
-  //   Bucket: "monster-of-the-week-ui",
-  //   Key: "serve-site-from-edge-lambda/",
-  // };
-  // const command = new GetObjectCommand(input);
-  // const client = new S3Client({ region: "us-east-1" });
+  const input: GetObjectCommandInput = {
+    Bucket: "monster-of-the-week-ui",
+    Key: "serve-site-from-edge-lambda/",
+  };
+  const command = new GetObjectCommand(input);
+  const client = new S3Client({ region: "us-east-1" });
 
-  // console.log("getting files");
+  console.log("getting files");
 
-  // const files = await client.send(command);
+  const files = await client.send(command);
 
-  // console.log("got files");
+  console.log("got files");
 
   const response = {
     status: 200,
     statusDescription: "OK",
-    body: "Lambda@Edge is awesome! Part 4!",
+    body: "Lambda@Edge is awesome! Part 5!",
   };
 
   return response;
