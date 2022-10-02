fe689898fabb7d11485f863b89fd249a9ff0d4db
evenflow58 <evenflow58@gmail.com>
2022-10-02 10:16:18 -0400

    Trying simple redirect

--- infrastructure/url/src/index.ts
+++ infrastructure/url/src/index.ts
@@ -1,30 +1,47 @@
-import {
-  S3Client,
-  GetObjectCommand,
-  GetObjectCommandInput,
-} from "@aws-sdk/client-s3";
+// import {
+//   S3Client,
+//   GetObjectCommand,
+//   GetObjectCommandInput,
+// } from "@aws-sdk/client-s3";
 
 export const handler = async (event: any): Promise<any> => {
   console.log(JSON.stringify(event));
 
-  const input: GetObjectCommandInput = {
-    Bucket: "monster-of-the-week-ui",
-    Key: "serve-site-from-edge-lambda/index.html",
+  const request = event.Records[0].cf.request;
+
+  console.log("request", request);
+
+  // const input: GetObjectCommandInput = {
+  //   Bucket: "monster-of-the-week-ui",
+  //   Key: "serve-site-from-edge-lambda/index.html",
+  // };
+  // const command = new GetObjectCommand(input);
+  // const client = new S3Client({ region: "us-east-1" });
+
+  // console.log("getting files");
+
+  // const file = await client.send(command);
+
+  // const response = {
+  //   status: 200,
+  //   statusDescription: "OK",
+  //   body: file.Body,
+  // };
+
+  // return response;
+
+  return {
+    status: "301",
+    statusDescription: "Found",
+    headers: {
+      location: [
+        {
+          key: "Location",
+          value: `https://${"serve-site-from-edge-lambda.monsterplaybook.rip"}/${
+            request.uri
+          }`,
+        },
+      ],
+    },
   };
-  const command = new GetObjectCommand(input);
-  const client = new S3Client({ region: "us-east-1" });
-
-  console.log("getting files");
-
-  const files = await client.send(command);
-
-  console.log("got files", JSON.stringify(files));
-
-  const response = {
-    status: 200,
-    statusDescription: "OK",
-    body: "Lambda@Edge is awesome! Part 5!",
-  };
-
-  return response;
 };
