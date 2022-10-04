d2a2eba153dcf1e1309bc0bfec6772db3af963dd
evenflow58 <evenflow58@gmail.com>
2022-10-04 17:11:56 -0400

    Added comments

--- infrastructure/url/src/index.ts
+++ infrastructure/url/src/index.ts
@@ -3,19 +3,14 @@ import path from "path";
 export const handler = async (event: any): Promise<any> => {
   const request = event.Records[0].cf.request;
 
-  console.log("original uri", request.uri);
-
+  // Add index.html if this is the base request.
   if (!path.extname(request.uri)) {
     console.log("changing path");
     request.uri = request.uri.replace(/\/?$/, "/index.html");
   }
 
-  console.log("new URI", request.uri);
-
-  // request.uri = `/serve-site-from-edge-lambda${request.uri}`;
+  // Adding the branch name to the request to get the correct version.
   request.uri = `/${request.origin.s3.customHeaders["x-env-branch"][0].value}${request.uri}`;
-
-  console.log("finished uri", request.uri);
 
   return request;
 };
