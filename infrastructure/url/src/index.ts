36db65927ee6b63bd11f96f3c512fc598be12876
evenflow58 <evenflow58@gmail.com>
2022-10-04 16:48:05 -0400

    Customer header and changing principal

--- infrastructure/url/src/index.ts
+++ infrastructure/url/src/index.ts
@@ -1,34 +1,9 @@
-// import {
-//   S3Client,
-//   GetObjectCommand,
-//   GetObjectCommandInput,
-// } from "@aws-sdk/client-s3";
-
 import path from "path";
 
 export const handler = async (event: any): Promise<any> => {
   const request = event.Records[0].cf.request;
 
   console.log("original uri", request.uri);
-
-  // const input: GetObjectCommandInput = {
-  //   Bucket: "monster-of-the-week-ui",
-  //   Key: "serve-site-from-edge-lambda/index.html",
-  // };
-  // const command = new GetObjectCommand(input);
-  // const client = new S3Client({ region: "us-east-1" });
-
-  // console.log("getting files");
-
-  // const file = await client.send(command);
-
-  // const response = {
-  //   status: 200,
-  //   statusDescription: "OK",
-  //   body: file.Body,
-  // };
-
-  // return response;
 
   if (!path.extname(request.uri)) {
     console.log("changing path");
@@ -37,7 +12,8 @@ export const handler = async (event: any): Promise<any> => {
 
   console.log("new URI", request.uri);
 
-  request.uri = `/serve-site-from-edge-lambda${request.uri}`;
+  // request.uri = `/serve-site-from-edge-lambda${request.uri}`;
+  request.uri = `/${request.origin.s3.customHeaders["x-env-branch"][0].value}${request.uri}`;
 
   console.log("finished uri", request.uri);
 
