c3f72f5d72cc891bab65c57211fae50f8d099d00
evenflow58 <evenflow58@gmail.com>
2022-12-19 09:29:16 -0500

    Trying to run as nested stack

--- api/test/get/src/main.rs
+++ api/test/get/src/main.rs
@@ -1,10 +1,10 @@
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response, LambdaEvent};
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: LambdaEvent<Value>) -> Result<Response<Body>, Error> {
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
 
     // Return something that implements IntoResponse.
